0879ec049cd01da54fb2090768275544eca9a964
yelich <chaconyelitza11@gmail.com>
2021-02-24 22:09:48 -0500

    First GitHub with PyCharm

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('Yelitza Chacon')
+    print('Test control version')
