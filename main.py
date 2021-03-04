9969b51f7d3064bac0f6b6b21a834dfc4ef89723
yelich <chaconyelitza11@gmail.com>
2021-03-03 21:22:44 -0500

    Practice hello_world.py print Even or Odd

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from datetime import password
+from Playingdate import today, todaytime
+from guessinggame import attempts
+from hello_world import num
+
 
 def print_hi(name):
     print(f'Hi, {name}')
 
-def my_fuction(a,b):
+
+def myfuction(a, b):
     return a + b
+
 
 if __name__ == '__main__':
     print_hi('Yelitza Chacon')
-    print('Test control version')
-    print(password)
-    print(my_fuction(2, 4))
+    print(myfuction(2, 4))
+    print(today)
+    print(todaytime)
