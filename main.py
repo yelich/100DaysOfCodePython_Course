d3e64c94ebd6ed73aa1e9f40852fd44dfebf2d86
yelich <chaconyelitza11@gmail.com>
2021-03-10 20:45:46 -0500

    Practice rpsgame.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from Playingdate import today, todaytime
 from guessinggame import attempts
 from hello_world import num
 
@@ -6,13 +5,6 @@ from hello_world import num
 def print_hi(name):
     print(f'Hi, {name}')
 
-
-def myfuction(a, b):
-    return a + b
-
-
 if __name__ == '__main__':
     print_hi('Yelitza Chacon')
-    print(myfuction(2, 4))
-    print(today)
-    print(todaytime)
+
